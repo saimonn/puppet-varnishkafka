ba373eec5fd882465185da916daee51cd63d8421
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2017-02-15 13:48:36 +0100

    Add dependency on install from config class

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,6 +5,7 @@ class varnishkafka::config {
     group   => 'root',
     mode    => '0644',
     content => template("${module_name}/varnishkafka.conf.erb"),
+    require => Class['varnishkafka::install'],
     notify  => Class['varnishkafka::service'],
   }
 }
